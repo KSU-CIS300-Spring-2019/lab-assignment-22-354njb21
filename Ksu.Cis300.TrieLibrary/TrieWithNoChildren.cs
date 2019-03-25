1f5be4ce69cd7c3fb3b93bd08bbce770ad0d798c
Nathan Brown <njbrown@ksu.edu>
2019-03-25 15:20:41 -0500

    Lab 22

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        private bool _empty = false;
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _empty = true;
+                return this;
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _empty);
+            }
+        }
+
+        public bool Contains(string s)
+        {
+            if (s != "")
+            {
+                return _empty;
+            }
+            return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _result = true;
+                return this;
+            }
+            else if (s[0] == _label)
+            {
+               _child = _child.Add(s.Substring(1));
+                return this;
+            }
+            else 
+            {
+                return new TrieWithManyChildren(s, _result, _label, _child);
+            }
+        }
+
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                _result = true;
+                return _result;
+            }
+            if (s[0] == _label)
+            {
+                bool check = _child.Contains(s);
+                return check;
+            }
+            return false;
+        }
+
+        private bool _result;
+        private ITrie _child;
+        private char _label; 
+
+
+        public TrieWithOneChild(string s, bool result)
+        {
+            if (s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+
+            _result = result;
+            _label = s[0];
+            _child = new TrieWithNoChildren();
+            _child.Add(s.Substring(1));
+        } 
+    }
+}
